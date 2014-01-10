fa180f30ac05906acc81e58a6a82afadc6fc6a4e
Maximov Maxim <maximov@saritasa.com>
2014-01-10 16:24:54 +0800

    added map

--- Application/droid-fake-road/src/mobi/droid/fakeroad/ui/activity/MainActivity.java
+++ Application/droid-fake-road/src/mobi/droid/fakeroad/ui/activity/MainActivity.java
@@ -1,16 +1,57 @@
 package mobi.droid.fakeroad.ui.activity;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLngBounds;
 import mobi.droid.fakeroad.R;
 
-public class MainActivity extends Activity{
-
+public class MainActivity extends FragmentActivity{
+    private GoogleMap mMap;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        setUpMapIfNeeded();
+    }
+
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMap() {
+        // Hide the zoom controls as the button panel will cover it.
+        mMap.getUiSettings().setZoomControlsEnabled(false);
+
+
+
+        // Pan to see all markers in view.
+        // Cannot zoom to bounds until the map has a size.
+        final View mapView = getSupportFragmentManager().findFragmentById(R.id.map).getView();
 
     }
 }
